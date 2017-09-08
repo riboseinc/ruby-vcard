1023b0fe8bfbaa0f48e2a81fa956da844ec2a72e
Nick Nicholas <opoudjis@optusnet.com.au>
2017-09-08 19:52:50 +1000

    testing

--- spec/lib/vcard_spec.rb
+++ spec/lib/vcard_spec.rb
@@ -57,9 +57,16 @@ describe Vcard do
       expect { Vcard.new('3.0').parse(ics.gsub(/\r\n?/,"\n"))}.to raise_error(Rsec::SyntaxError)
   end 
 
-  it 'should reject X-parameters on IMPP' do
+  it 'should reject X-parameters on IMPP in v3' do
       ics = File.read "spec/examples/apple4.vcf"
       expect { Vcard.new('3.0').parse(ics.gsub(/\r\n?/,"\n"))}.to raise_error(Rsec::SyntaxError)
+  end 
+
+  it 'should not reject X-parameters on IMPP in v4' do
+      ics = File.read "spec/examples/apple5.vcf"
+      vobj_json = Vcard.new('4.0').parse(ics.gsub(/\r\n?/,"\n")).to_json
+      exp_json = JSON.load(File.read "spec/examples/apple5.json")
+      expect(vobj_json).to include_json(exp_json)
   end 
 
   it 'should process VCF from Apple' do
@@ -74,17 +81,24 @@ describe Vcard do
       expect { Vcard.new('3.0').parse(ics.gsub(/\r\n?/,"\n"))}.to raise_error(Rsec::SyntaxError)
   end
 
-  it 'should reject TYPE parameter on X-property' do
+  it 'should reject TYPE parameter on X-property in v3' do
       ics = File.read "spec/examples/ujb2.vcf"
       expect { Vcard.new('3.0').parse(ics.gsub(/\r\n?/,"\n"))}.to raise_error(Rsec::SyntaxError)
   end
 
-  it 'should reject VCF with FN but no N' do
+  it 'should not reject TYPE parameter on X-property in v4' do
+      ics = File.read "spec/examples/ujb3.vcf"
+      vobj_json = Vcard.new('4.0').parse(ics.gsub(/\r\n?/,"\n")).to_json
+      exp_json = JSON.load(File.read "spec/examples/ujb3.json")
+      expect(vobj_json).to include_json(exp_json)
+  end
+
+  it 'should reject VCF with FN but no N in v3' do
       ics = File.read "spec/examples/example51.vcf"
       expect { Vcard.new('3.0').parse(ics.gsub(/\r\n?/,"\n"))}.to raise_error(Rsec::SyntaxError)
   end
 
-  it 'should reject VCF with FN but no N' do
+  it 'should reject VCF with FN but no N in v3' do
       ics = File.read "spec/examples/example61.vcf"
       expect { Vcard.new('3.0').parse(ics.gsub(/\r\n?/,"\n"))}.to raise_error(Rsec::SyntaxError)
   end
@@ -140,7 +154,7 @@ describe Vcard do
       expect { Vcard.new('4.0').parse(ics.gsub(/\r\n?/,"\n"))}.to raise_error(Rsec::SyntaxError)
   end
 
-    it 'should reject TYPE param on X-name property' do
+    it 'should reject TYPE param on X-name property in v3' do
       ics = File.read "spec/examples/John_Doe_EVOLUTION.vcf"
       expect { Vcard.new('3.0').parse(ics.gsub(/\r\n?/,"\n"))}.to raise_error(Rsec::SyntaxError)
     end
